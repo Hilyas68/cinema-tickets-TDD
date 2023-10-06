033bf2b60257ccec85dc88490e2c0d73e966133d
Hassan Liasu <hassan.liasu@engineering.digital.dwp.gov.uk>
2023-10-06 12:15:41 +0100

    Given an invalid account ID then throw an exception : parameterized test

--- src/test/java/TicketServiceTest.java
+++ src/test/java/TicketServiceTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import uk.gov.dwp.uc.pairtest.TicketService;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
@@ -18,12 +20,13 @@ public class TicketServiceTest {
     ticketService = new TicketServiceImpl();
   }
 
-  @Test
+  @ParameterizedTest
+  @CsvSource({"0", "-1"})
   @DisplayName("Given an invalid account ID then throw an exception")
-  public void givenInvalidAccountIdThrowException() {
+  public void givenInvalidAccountIdThrowException(long input) {
 
     InvalidPurchaseException exception = assertThrows(InvalidPurchaseException.class,
-        () -> ticketService.purchaseTickets(0L));
+        () -> ticketService.purchaseTickets(input));
 
     assertEquals(exception.getMessage(), "Invalid Account ID", "should return invalid account ID");
   }
