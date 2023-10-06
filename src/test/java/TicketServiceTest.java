e57046a02f63a99583bb45d4879633d87ce10635
Hassan Liasu <hassan.liasu@engineering.digital.dwp.gov.uk>
2023-10-06 12:09:22 +0100

    Given an invalid account ID then throw an exception

--- src/test/java/TicketServiceTest.java
+++ src/test/java/TicketServiceTest.java
@@ -1,5 +1,27 @@
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import uk.gov.dwp.uc.pairtest.TicketService;
+import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
 public class TicketServiceTest {
 
 
+  private TicketService ticketService;
+
+  @BeforeEach
+  public void setup() {
+    ticketService = new TicketServiceImpl();
+  }
+
+  @Test
+  @DisplayName("Given an invalid account ID then throw an exception")
+  public void givenInvalidAccountIdThrowException() {
+
+    assertThrows(InvalidPurchaseException.class, () -> ticketService.purchaseTickets(0L));
+  }
 
 }
