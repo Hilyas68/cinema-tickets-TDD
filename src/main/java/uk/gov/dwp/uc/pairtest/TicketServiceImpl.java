fc5a27b54ef577b1580e14a2b9460e60542a3af3
Hassan Liasu <hassan.liasu@engineering.digital.dwp.gov.uk>
2023-10-06 12:35:07 +0100

    Given a valid accountID and an empty ticket,then no payment and reservation is made

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,5 +1,7 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
@@ -8,9 +10,18 @@ public class TicketServiceImpl implements TicketService {
   public static final int MIN_ACCOUNT_ID = 1;
   public static final String INVALID_ACCOUNT_ID_MESSAGE = "Invalid Account ID";
 
+  private final TicketPaymentService paymentService;
+  private final SeatReservationService reservationService;
+
   /**
    * Should only have private methods other than the one below.
    */
+
+  public TicketServiceImpl(TicketPaymentService paymentService,
+      SeatReservationService reservationService) {
+    this.paymentService = paymentService;
+    this.reservationService = reservationService;
+  }
 
   @Override
   public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests)
@@ -18,6 +29,11 @@ public class TicketServiceImpl implements TicketService {
 
     if (accountId < MIN_ACCOUNT_ID) {
       throw new InvalidPurchaseException(INVALID_ACCOUNT_ID_MESSAGE);
+    }
+
+    if (ticketTypeRequests.length > 0) {
+      paymentService.makePayment(accountId, -1);
+      reservationService.reserveSeat(accountId, -1);
     }
   }
 
--- src/test/java/TicketServiceTest.java
+++ src/test/java/TicketServiceTest.java
@@ -1,23 +1,38 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.TicketService;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
+@ExtendWith(MockitoExtension.class)
 public class TicketServiceTest {
 
+  @Mock
+  private TicketPaymentService paymentService;
+
+  @Mock
+  private SeatReservationService reservationService;
 
   private TicketService ticketService;
 
   @BeforeEach
   public void setup() {
-    ticketService = new TicketServiceImpl();
+    ticketService = new TicketServiceImpl(paymentService, reservationService);
   }
 
   @ParameterizedTest
@@ -31,4 +46,13 @@ public class TicketServiceTest {
     assertEquals(exception.getMessage(), "Invalid Account ID", "should return invalid account ID");
   }
 
+  @Test
+  @DisplayName("Given a valid accountID and an empty ticket,then no payment and reservation is made")
+  public void givenEmptyTicket() {
+
+    ticketService.purchaseTickets(1L);
+
+    verify(paymentService, never()).makePayment(anyLong(), anyInt());
+    verify(reservationService, never()).reserveSeat(anyLong(), anyInt());
+  }
 }
