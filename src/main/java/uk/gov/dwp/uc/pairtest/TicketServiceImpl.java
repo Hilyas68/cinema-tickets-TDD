5b090a9c7c1f53cc692f70797f58521268163822
Hassan Liasu <hassan.liasu@engineering.digital.dwp.gov.uk>
2023-10-06 12:14:10 +0100

    Given an invalid account ID then throw an exception : refactor

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -6,6 +6,7 @@ import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 public class TicketServiceImpl implements TicketService {
 
   public static final int MIN_ACCOUNT_ID = 1;
+  public static final String INVALID_ACCOUNT_ID_MESSAGE = "Invalid Account ID";
 
   /**
    * Should only have private methods other than the one below.
@@ -16,7 +17,7 @@ public class TicketServiceImpl implements TicketService {
       throws InvalidPurchaseException {
 
     if (accountId < MIN_ACCOUNT_ID) {
-      throw new InvalidPurchaseException("Invalid Account ID");
+      throw new InvalidPurchaseException(INVALID_ACCOUNT_ID_MESSAGE);
     }
   }
 
